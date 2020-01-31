01693cb807abd7bfc3f4685b5714876ef581564b
HusooTV <53692864+HusooTV@users.noreply.github.com>
2020-01-31 17:34:35 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('https://discord.gg/fZ23Cqs Selamlar,Değerli **' + username + '** Discord Sunucumaza Hoşgeldin :kissing_heart: #Duyuru kanalına bakmayı unutma; sunucuda olan duyuruları öğrenebilirsin ,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle. Buda sunucumuzun sınırsız linki. :wink:');
+    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucumaza Hoşgeldin :kissing_heart:  :thinking: Keyifli Vakit Geçirmen Dileğiyle. Buda sunucumuzun sınırsız linki. :wink:');
 
 };
