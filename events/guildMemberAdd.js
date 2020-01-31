fb02601d5577342fe6017abd14595d4feee755c0
HusooTV <53692864+HusooTV@users.noreply.github.com>
2020-01-31 15:38:17 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://discord.gg/fZ23Cqs Selamlar,Değerli **' + username + '** Discord Sunucumaza Hoşgeldin :kissing_heart: #Duyuru kanalına bakmayı unutma; sunucuda olan duyuruları öğrenebilirsin ,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle. Buda sunucumuzun sınırsız linki. :wink:');
+
+};
